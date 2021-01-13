afad11a05514b16e5276cedab62f0664fdf15428
Danylo <tomahawk535@gmail.com>
2021-01-13 12:46:59 +0200

    correct counter

--- SmartDevelopers/js/main.js
+++ SmartDevelopers/js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
-    $('#coundown').countdown('2017/10/10').on('update.countdown', function(event) {
+    $('#coundown').countdown('2021/03/17').on('update.countdown', function(event) {
         var $this = $(this).html(event.strftime(''
-            + '<span>%-d</span> дней'
+            + '<span>%D</span> дней'
             + '<span>%H</span> часов '
             + '<span>%M</span> минут '
             + '<span>%S</span> секунд'));
